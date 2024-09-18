771c7d97f3becd5f95b63e28bc8afc1a4569ce35
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-09-18 12:14:03 +0300

    added length validation for coment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,7 +13,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 280 }
   validates :user_id, presence: true
   validates :tweet_id, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Comment, type: :model do
   it { should belong_to(:tweet) }
 
   it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_most(280) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:tweet_id) }
 end
