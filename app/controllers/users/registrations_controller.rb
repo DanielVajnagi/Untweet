6c2cd35cd7b258ed3a322dee2a36976a38d04846
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2024-10-09 13:56:38 +0300

    changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,10 +7,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
   end
-
-
 end
