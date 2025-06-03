915bfa04479c220d8318e99738a8e582c1563683
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2025-06-04 01:45:21 +0300

    Localization (#56)
    * update readme
    
    * localization
    
    * localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: "users/sessions"
-  }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Set locale from URL
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users, controllers: {
+      registrations: "users/registrations",
+      sessions: "users/sessions"
+    }
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+    # Render dynamic PWA files from app/views/pwa/*
+    get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  root "tweets#index"
+    # Defines the root path route ("/")
+    root "tweets#index"
 
-  resources :tweets do
-    member do
-      post :retweet
-      get  :new_quote
-      post :create_quote
+    resources :tweets do
+      member do
+        post :retweet
+        get  :new_quote
+        post :create_quote
+      end
     end
   end
 
+  # Redirect root to default locale
+  get '/', to: redirect("/#{I18n.default_locale}")
 end
