71c5c3ed184a9441dab5499304092797b5d9214f
DanielVajnagi <82052651+DanielVajnagi@users.noreply.github.com>
2025-06-04 01:44:41 +0300

    localization

--- app/controllers/concerns/locale_switcher.rb
+++ app/controllers/concerns/locale_switcher.rb
@@ -8,22 +8,32 @@ module LocaleSwitcher
   private
 
   def set_locale
-    Rails.logger.debug "Locale configuration:"
-    Rails.logger.debug "  Default locale: #{I18n.default_locale}"
-    Rails.logger.debug "  Available locales: #{I18n.available_locales}"
-    Rails.logger.debug "  Extracted locale: #{extract_locale}"
-    Rails.logger.debug "  Current locale before: #{I18n.locale}"
+    Rails.logger.debug "Default locale: #{I18n.default_locale}"
+    Rails.logger.debug "Available locales: #{I18n.available_locales}"
 
-    I18n.locale = extract_locale || I18n.default_locale
+    # Extract locale from URL
+    extracted_locale = extract_locale_from_url
+    Rails.logger.debug "Extracted locale from URL: #{extracted_locale}"
 
-    Rails.logger.debug "  Current locale after: #{I18n.locale}"
+    # Set locale based on URL parameter or default
+    I18n.locale = extracted_locale || I18n.default_locale
+    Rails.logger.debug "Current locale before setting: #{I18n.locale}"
+
+    # Store the current path for redirection
+    @current_path = request.fullpath.gsub(/^\/#{extracted_locale}/, '') if extracted_locale
+
+    # Redirect to default locale if no locale specified
+    if extracted_locale.nil? && request.path == '/'
+      Rails.logger.debug "Redirecting to default locale: #{I18n.default_locale}"
+      redirect_to "/#{I18n.default_locale}"
+    end
   end
 
-  def extract_locale
-    parsed_locale = params[:locale]
-    return nil unless parsed_locale
-    return nil unless I18n.available_locales.map(&:to_s).include?(parsed_locale)
-    parsed_locale
+  def extract_locale_from_url
+    locale = params[:locale]
+    return nil unless locale
+    return nil unless I18n.available_locales.include?(locale.to_sym)
+    locale
   end
 
   def default_url_options
